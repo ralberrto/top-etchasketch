e0dae55e4c9738814dcc4e5c7ccaf90da2fdb437
Rafael Alberto Larios <albertolarios@me.com>
2022-07-03 13:51:18 -0500

    Add on-hover classes, add style.css file
    The classes were added through event listeners.

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 console.log("Hello world!")
 const cont = document.querySelector("#grid-container");
 
-const gridSize = 4;
+const gridSize = 100;
+const elements = Array(0).fill(undefined);
+
+// Creates the elements conforming the grid
 for (let row = 0; row < gridSize; row++) {
   let rowElement = document.createElement("div")
   rowElement.classList.add("row")
   cont.appendChild(rowElement)
   for (let col = 0; col < gridSize; col++) {
-    let colElement = document.createElement("div");
-    colElement.classList.add("column");
-    rowElement.appendChild(colElement);
+    let element = document.createElement("div");
+    element.classList.add("element");
+    elements.push(element);
+    rowElement.appendChild(element);
   }
 }
+
+elements.forEach(element => element.addEventListener("mouseover",
+    () => element.classList.add("hovered-on")))
