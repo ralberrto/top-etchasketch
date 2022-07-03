0788c36fb5f7caa709e286cb87b28e6681345f0c
Rafael Alberto Larios <albertolarios@me.com>
2022-07-03 18:19:07 -0500

    Add button to resize content; adjust js to enable button
    Styling was added to make the button prettier.

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 console.log("Hello world!")
-const cont = document.querySelector("#grid-container");
 
-const gridSize = 100;
-const elements = Array(0).fill(undefined);
+const gridContainer = document.querySelector("#grid-container");
 
-// Creates the elements conforming the grid
-for (let row = 0; row < gridSize; row++) {
-  let rowElement = document.createElement("div")
-  rowElement.classList.add("row")
-  cont.appendChild(rowElement)
-  for (let col = 0; col < gridSize; col++) {
-    let element = document.createElement("div");
-    element.classList.add("element");
-    elements.push(element);
-    rowElement.appendChild(element);
+const resolutionButton = document.querySelector("nav button");
+resolutionButton.addEventListener("click", () => drawGrid(changeGridSize()));
+
+drawGrid(64)
+
+function changeGridSize() {
+  let userGridSize = prompt("Type resolution:");
+  if (userGridSize > 100) {
+    confirm("The size must be less or equal to 100!");
+    userGridSize = changeGridSize();
   }
+  return userGridSize;
 }
 
-elements.forEach(element => element.addEventListener("mouseover",
-    () => element.classList.add("hovered-on")))
+function drawGrid(gridSize) {
+  clearGrid()
+  const elements = Array(0).fill(undefined);
+  
+  // Creates the elements conforming the grid
+  for (let row = 0; row < gridSize; row++) {
+    let rowElement = document.createElement("div")
+    rowElement.classList.add("row")
+    gridContainer.appendChild(rowElement)
+    for (let col = 0; col < gridSize; col++) {
+      let element = document.createElement("div");
+      element.classList.add("element");
+      elements.push(element);
+      rowElement.appendChild(element);
+    }
+  }
+  elements.forEach(element => element.addEventListener("mouseover",
+    () => element.classList.add("hovered-on")));
+}
+
+function clearGrid() {
+  gridContainer.textContent = "";
+}
+
